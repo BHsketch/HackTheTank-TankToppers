5f484a70d98df4cf6b15d88477e582074d000a6e
thehthakur <thehthakur@gmail.com>
2023-07-10 17:17:27 +0530

    Use fetch API in place of axios and other small changes to reflect the new structure.

--- user-journey-react/src/App.jsx
+++ user-journey-react/src/App.jsx
@@ -8,62 +8,26 @@ import ReactFlow, {
   addEdge,
 } from "reactflow";
 
+import Graph from "./components/graph/graph.component";
+
 import "reactflow/dist/style.css";
 
-var labelThis = 1;
-
-const initialNodes = [
-  { id: "1", position: { x: 0, y: 0 }, data: { label: labelThis } },
-  { id: "2", position: { x: 0, y: 100 }, data: { label: "2" } },
-];
-const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
-
 export default function App() {
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
-  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
-  const [nodeName, setNodeName] = useState("1");
+  const [labelList, setLabelList] = useState([]);
+
   useEffect(() => {
-    setNodes((nds) =>
-      nds.map((node) => {
-        if (node.id === "1") {
-          node.data = {
-            ...node.data,
-            label: nodeName,
-          };
-        }
-
-        return node;
-      })
-    );
-  }, [nodeName, setNodes]);
-
-  const onConnect = useCallback(
-    (params) => setEdges((eds) => addEdge(params, eds)),
-    [setEdges, labelThis]
-  );
+    try {
+      fetch("https://jsonplaceholder.typicode.com/albums")
+        .then((response) => response.json())
+        .then((albums) => setLabelList(albums));
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-      >
-        <Controls />
-        <MiniMap />
-        <Background variant="dots" gap={12} size={1} />
-      </ReactFlow>
-      <button
-        onClick={() => {
-          labelThis += 1;
-          setNodeName(labelThis);
-          console.log(labelThis);
-        }}
-      >
-        Click to increment
-      </button>
+    <div className="App">
+      <Graph dataFromDB={labelList} />
     </div>
   );
 }
