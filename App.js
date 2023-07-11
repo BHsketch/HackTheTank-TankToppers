549fa35f7a3c28d512bc1286f7991b1a0e95265d
Param Shah <96541661+paramshah1903@users.noreply.github.com>
2023-07-11 18:20:41 +0530

    Add files via upload
    labels updating dynamically

--- App.js
+++ App.js
@@ -2,29 +2,126 @@ import "./App.css";
 import Graph from "./Graph";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
+import "./Loader.css";
 
 function App() {
-  const [scholarshipId, setScholarshipId] = useState(null); // Declare scholarshipId in the component's state
+  const [initialState, setInitialState] = useState({
+    userCount: 0,
+    loginCount: 0,
+    failedLoginCount: 0,
+    contactCount: 0,
+    aboutCount: 0,
+    scholarshipListingCount: 0,
+    homeCount: 0,
+    scholarshipCount: 0,
+    bookmarkCount: 0,
+    applyCount: 0,
+    termsCount: 0,
+    ineligibleCount: 0,
+    incompleteCount: 0,
+    submittedCount: 0,
+    failedCount: 0,
+    successCount: 0,
+    submittedUsers: [],
+  });
+
+  // const [submittedUsers, setSubmittedUsers] = useState();
 
   useEffect(() => {
-    const fetchAllBooks = async () => {
+    const fetchUsersCount = async (s1) => {
       try {
-        const books = await axios.get("http://localhost:8800/books");
-        const fetchedScholarshipId = books.data[0].id;
-        setScholarshipId(fetchedScholarshipId); // Update scholarshipId in the state
+        const p1 = await axios.get("http://localhost:8800/users");
+        const response1 = p1.data[0].user_count;
+        const p = await axios.get("http://localhost:8800/pages");
+        const response = p.data;
+        const count = await axios.get(
+          `http://localhost:8800/scholarshipCount/s1`
+        );
+        const ans = count.data.scholarshipCount;
+        // console.log(ans);
+        const bookmark = await axios.get("http://localhost:8800/s1/bookmark");
+        const applied = await axios.get("http://localhost:8800/s1/applied");
+        const terms = await axios.get("http://localhost:8800/s1/terms");
+        const ineligible = await axios.get(
+          "http://localhost:8800/s1/ineligible"
+        );
+        const incomplete = await axios.get(
+          "http://localhost:8800/s1/incomplete"
+        );
+        const submitted = await axios.get("http://localhost:8800/s1/submitted");
+        const failed = await axios.get("http://localhost:8800/s1/failed");
+        const success = await axios.get("http://localhost:8800/s1/success");
+        console.log(submitted.data.users);
+        console.log(terms.data.userCount);
+        setInitialState({
+          ...initialState,
+          userCount: response1,
+          loginCount: response.p1,
+          homeCount: response.p2,
+          failedLoginCount: response.p1 - response.p2,
+          contactCount: response.p3,
+          scholarshipListingCount: response.p4,
+          aboutCount: response.p5,
+          scholarshipCount: ans,
+          bookmarkCount: bookmark.data.userCount,
+          applyCount: applied.data.userCount,
+          termsCount: terms.data.userCount,
+          ineligibleCount: ineligible.data.userCount,
+          incompleteCount: incomplete.data.userCount,
+          submittedCount: submitted.data.userCount,
+          failedCount: failed.data.userCount,
+          successCount: success.data.userCount,
+          submittedUsers: submitted.data.users,
+        });
+        // setSubmittedUsers(submitted.data.users);
+        // console.log(initialState);
       } catch (error) {
         console.log(error);
       }
     };
-    fetchAllBooks();
+    fetchUsersCount();
   }, []);
 
   return (
     <div className="App">
-      {scholarshipId && <Graph scholarshipId={scholarshipId} />}{" "}
-      {/* Render the Graph component once scholarshipId is available */}
+      <Router>
+        <Routes>
+          <Route
+            path="/:scholarshipId"
+            element={
+              initialState.userCount !== 0 ? (
+                <Graph data={initialState} />
+              ) : (
+                <div class="custom-loader"></div>
+              )
+            }
+          />
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+//   return (
+//     <div className="App">
+//       <Router>
+//         <Routes>
+//           <Route
+//             path="/:scholarshipId"
+//             element={
+//               initialState.userCount !== 0 && <Graph data={initialState} />
+//             }
+//           />
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
--- Graph.js
+++ Graph.js
@@ -1,5 +1,13 @@
-import React, { useCallback } from "react";
-import ReactFlow, { useNodesState, useEdgesState, addEdge } from "reactflow";
+import React, { useCallback, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+import ReactFlow, {
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  Panel,
+} from "reactflow";
 
 import "reactflow/dist/style.css";
 
@@ -18,320 +26,401 @@ const customEdgeStyles = {
   stroke: "#FF0072",
 };
 
-const initialNodes = [
-  {
-    id: "1",
-    position: { x: 25, y: 300 },
-    data: { label: "Login Page 100%" },
-    style: {
-      ...commonStyles,
-    },
-    type: "input",
-    sourcePosition: "right",
-  },
-  {
-    id: "2",
-    position: { x: 300, y: 300 },
-    data: { label: `Home Page 99%` },
-    type: "default",
-    targetPosition: "left",
-    style: {
-      ...commonStyles,
-    },
-    sourcePosition: "right",
-  },
-  {
-    id: "3",
-    position: { x: 500, y: 100 },
-    data: { label: `Contact Us 8%` },
-    type: "default",
-    targetPosition: "left",
-    style: {
-      ...commonStyles,
-    },
-    sourcePosition: "right",
-  },
-  {
-    id: "4",
-    position: { x: 500, y: 300 },
-    data: { label: `Scholarships 89%` },
-    type: "default",
-    targetPosition: "left",
-    sourcePosition: "right",
-    style: {
-      ...commonStyles,
-    },
-  },
-  {
-    id: "5",
-    position: { x: 500, y: 500 },
-    data: { label: `About us 2%` },
-    type: "default",
-    targetPosition: "left",
-    style: {
-      ...commonStyles,
-    },
-    sourcePosition: "right",
-  },
-  {
-    id: "p",
-    type: "group",
-    // type:"output"
-    data: { label: null },
-    position: { x: 700, y: 50 },
-    style: {
-      width: 700,
-      height: 600,
-    },
-    targetPosition: "left",
-  },
-  {
-    id: "6",
-    data: { label: "Total Views of Scholarship(100%)" },
-    position: { x: 10, y: 250 },
-    parentNode: "p",
-    extent: "parent",
-    type: "default",
-    targetPosition: "left",
-    style: {
-      ...commonStyles,
-      height: "100px",
-      width: "100px",
-    },
-    sourcePosition: "right",
-  },
-  {
-    id: "7",
-    data: { label: "Bookmarked" },
-    position: { x: 150, y: 50 },
-    parentNode: "p",
-    extent: "parent",
-    type: "default",
-    // draggable: false,
-    style: {
-      ...commonStyles,
-    },
-    targetPosition: "left",
-    sourcePosition: "right",
-  },
-  {
-    id: "8",
-    data: { label: "Applied" },
-    position: { x: 150, y: 200 },
-    parentNode: "p",
-    extent: "parent",
-    type: "default",
-    draggable: false,
-    style: {
-      ...commonStyles,
-    },
-    targetPosition: "left",
-    sourcePosition: "right",
-  },
-  {
-    id: "9",
-    data: { label: "Terms & Conditions Inquiry" },
-    position: { x: 150, y: 350 },
-    parentNode: "p",
-    extent: "parent",
-    type: "default",
-    draggable: false,
-    style: {
-      ...commonStyles,
-    },
-    targetPosition: "left",
-    sourcePosition: "right",
-  },
-  {
-    id: "10",
-    data: { label: "Not eligible for scholarship" },
-    position: { x: 150, y: 500 },
-    parentNode: "p",
-    extent: "parent",
-    type: "default",
-    draggable: false,
-    style: {
-      ...commonStyles,
-    },
-    targetPosition: "left",
-    sourcePosition: "right",
-  },
-  {
-    id: "11",
-    data: { label: "Saved incomplete application" },
-    position: { x: 350, y: 100 },
-    parentNode: "p",
-    extent: "parent",
-    type: "default",
-    draggable: false,
-    style: {
-      ...commonStyles,
-    },
-    targetPosition: "left",
-    sourcePosition: "right",
-  },
-  {
-    id: "12",
-    data: { label: "Submitted application" },
-    position: { x: 350, y: 300 },
-    parentNode: "p",
-    extent: "parent",
-    type: "default",
-    // draggable: false,
-    style: {
-      ...commonStyles,
-    },
-    targetPosition: "left",
-    sourcePosition: "right",
-  },
-  {
-    id: "13",
-    data: { label: "Failed Document Verification" },
-    position: { x: 500, y: 200 },
-    parentNode: "p",
-    extent: "parent",
-    // draggable: false,
-    type: "default",
-    style: {
-      ...commonStyles,
-      backgroundColor: "red",
-      color: "white",
-    },
-    targetPosition: "left",
-    sourcePosition: "right",
-  },
-  {
-    id: "14",
-    data: { label: "Successfully Applied" },
-    position: { x: 500, y: 400 },
-    parentNode: "p",
-    extent: "parent",
-    type: "default",
-    // draggable: false,
-    style: {
-      ...commonStyles,
-      backgroundColor: "green",
-      color: "white",
-    },
-    targetPosition: "left",
-    sourcePosition: "right",
-  },
-];
-const initialEdges = [
-  {
-    id: "e1-2",
-    source: "1",
-    target: "2",
-    style: {
-      ...customEdgeStyles,
-    },
-  },
-  {
-    id: "e2-3",
-    source: "2",
-    target: "3",
-    style: {
-      ...customEdgeStyles,
-    },
-  },
-  {
-    id: "e2-4",
-    source: "2",
-    target: "4",
-    style: {
-      ...customEdgeStyles,
-    },
-  },
-  {
-    id: "e2-5",
-    source: "2",
-    target: "5",
-    style: {
-      ...customEdgeStyles,
-    },
-  },
-  {
-    id: "e6-7",
-    source: "6",
-    target: "7",
-    style: {
-      ...customEdgeStyles,
-    },
-  },
-  {
-    id: "e6-8",
-    source: "6",
-    target: "8",
-    style: {
-      ...customEdgeStyles,
-    },
-  },
-  {
-    id: "e6-9",
-    source: "6",
-    target: "9",
-    style: {
-      ...customEdgeStyles,
-    },
-  },
-  {
-    id: "e6-10",
-    source: "6",
-    target: "10",
-    style: {
-      ...customEdgeStyles,
-    },
-  },
-  {
-    id: "e8-11",
-    source: "8",
-    target: "11",
-    style: {
-      ...customEdgeStyles,
-    },
-  },
-  {
-    id: "e8-12",
-    source: "8",
-    target: "12",
-    style: {
-      ...customEdgeStyles,
-    },
-  },
-  {
-    id: "e12-13",
-    source: "12",
-    target: "13",
-    style: {
-      ...customEdgeStyles,
-    },
-  },
-  {
-    id: "e12-14",
-    source: "12",
-    target: "14",
-    style: {
-      ...customEdgeStyles,
-    },
-  },
-  {
-    id: "e",
-    source: "4",
-    target: "6",
-    // animated: "true",
-    label: "Scholarship 1",
-    style: {
-      ...customEdgeStyles,
-    },
-  },
-  // { id: "e13-parent", source: "13", target: "parent" },
-];
+export default function Graph({ data }) {
+  // console.log(data.submittedUsers);
+  const [open, setOpen] = useState(false);
+  const { scholarshipId } = useParams();
+  const [scholarshipCount, setScholarshipCount] = useState(0);
+  // console.log(scholarshipId);
+  // console.log(data);
+  // const [initialState, setInitialState] = useState({
+  //   scholarshipCount: 0,
+  //   bookmarkCount: 0,
+  //   appliedCount: 0,
+  //   termsCount: 0,
+  //   ineligibleCount: 0,
+  //   incompleteCount: 0,
+  //   submittedCount: 0,
+  //   failedCount: 0,
+  //   successCount: 0,
+  // });
 
-const rfStyle = {
-  backgroundColor: "#D0C0F7",
-};
+  // const fetchScholarshipCount = async (scholarshipId) => {
+  //   try {
+  //     const response = await axios.get(
+  //       `http://localhost:8800/scholarshipCount/${scholarshipId}`
+  //     );
+  //     const ans = response.data.scholarshipCount;
+  //     console.log(ans);
+  //     // console.log(initialState);
+  //     setInitialState((prevState) => ({
+  //       ...prevState,
+  //       scholarshipCount: ans,
+  //     }));
 
-export default function Graph() {
+  //     console.log(initialState);
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // };
+  // useEffect(() => {
+  //   fetchScholarshipCount(scholarshipId); // Replace "s1" with the desired scholarshipId
+  // }, []);
+  // useEffect(() => {
+  //   const fetchScholarshipCount = async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         `http://localhost:8800/scholarshipCount/${scholarshipId}`
+  //       );
+  //       const ans = response.data.scholarshipCount;
+  //       console.log(ans);
+  //       setScholarshipCount(ans);
+  //     } catch (error) {
+  //       console.error(error);
+  //     }
+  //   };
+
+  //   fetchScholarshipCount();
+  // }, [scholarshipId]);
+
+  const initialNodes = [
+    {
+      id: "1",
+      position: { x: 25, y: 300 },
+      data: { label: `Login Page ${data.loginCount}` },
+      style: {
+        ...commonStyles,
+      },
+      onNodeClick: () => {
+        setOpen(true);
+      },
+      type: "input",
+      sourcePosition: "right",
+    },
+    {
+      id: "2",
+      position: { x: 300, y: 300 },
+      data: { label: `Home Page ${data.homeCount} ` },
+      type: "default",
+      targetPosition: "left",
+      style: {
+        ...commonStyles,
+      },
+      sourcePosition: "right",
+    },
+    {
+      id: "new",
+      position: { x: 300, y: 500 },
+      data: { label: `Failed Login ${data.failedLoginCount} ` },
+      type: "default",
+      targetPosition: "left",
+      style: {
+        ...commonStyles,
+        backgroundColor: "red",
+        color: "white",
+      },
+      sourcePosition: "right",
+    },
+    {
+      id: "3",
+      position: { x: 500, y: 100 },
+      data: { label: `Contact Us ${data.contactCount}` },
+      type: "default",
+      targetPosition: "left",
+      style: {
+        ...commonStyles,
+      },
+      sourcePosition: "right",
+    },
+    {
+      id: "4",
+      position: { x: 500, y: 300 },
+      data: { label: `Scholarships ${data.scholarshipListingCount} ` },
+      type: "default",
+      targetPosition: "left",
+      sourcePosition: "right",
+      style: {
+        ...commonStyles,
+      },
+    },
+    {
+      id: "5",
+      position: { x: 500, y: 500 },
+      data: { label: `About us ${data.aboutCount}` },
+      type: "default",
+      targetPosition: "left",
+      style: {
+        ...commonStyles,
+      },
+      sourcePosition: "right",
+    },
+    {
+      id: "p",
+      type: "group",
+      // type:"output"
+      data: { label: null },
+      position: { x: 700, y: 50 },
+      style: {
+        width: 700,
+        height: 600,
+      },
+      targetPosition: "left",
+    },
+    {
+      id: "6",
+      data: {
+        label: `Total Views of ${scholarshipId}:${data.scholarshipCount}`,
+      },
+      position: { x: 10, y: 250 },
+      parentNode: "p",
+      extent: "parent",
+      type: "default",
+      targetPosition: "left",
+      style: {
+        ...commonStyles,
+        height: "100px",
+        width: "100px",
+      },
+      sourcePosition: "right",
+    },
+    {
+      id: "7",
+      data: { label: `Bookmarked ${data.bookmarkCount}` },
+      position: { x: 150, y: 50 },
+      parentNode: "p",
+      extent: "parent",
+      type: "default",
+      // draggable: false,
+      style: {
+        ...commonStyles,
+      },
+      targetPosition: "left",
+      sourcePosition: "right",
+    },
+    {
+      id: "8",
+      data: { label: `Applied ${data.applyCount}` },
+      position: { x: 150, y: 200 },
+      parentNode: "p",
+      extent: "parent",
+      type: "default",
+      draggable: false,
+      style: {
+        ...commonStyles,
+      },
+      targetPosition: "left",
+      sourcePosition: "right",
+    },
+    {
+      id: "9",
+      data: { label: `Terms & Conditions Inquiry ${data.termsCount}` },
+      position: { x: 150, y: 350 },
+      parentNode: "p",
+      extent: "parent",
+      type: "default",
+      draggable: false,
+      style: {
+        ...commonStyles,
+      },
+      targetPosition: "left",
+      sourcePosition: "right",
+    },
+    {
+      id: "10",
+      data: { label: `Not eligible for scholarship ${data.ineligibleCount}` },
+      position: { x: 150, y: 500 },
+      parentNode: "p",
+      extent: "parent",
+      type: "default",
+      draggable: false,
+      style: {
+        ...commonStyles,
+      },
+      targetPosition: "left",
+      sourcePosition: "right",
+    },
+    {
+      id: "11",
+      data: { label: `Saved incomplete application ${data.incompleteCount}` },
+      position: { x: 350, y: 100 },
+      parentNode: "p",
+      extent: "parent",
+      type: "default",
+      draggable: false,
+      style: {
+        ...commonStyles,
+      },
+      targetPosition: "left",
+      sourcePosition: "right",
+    },
+    {
+      id: "12",
+      data: { label: `Submitted application ${data.submittedCount}` },
+      position: { x: 350, y: 300 },
+      parentNode: "p",
+      extent: "parent",
+      type: "default",
+      // draggable: false,
+      style: {
+        ...commonStyles,
+      },
+      targetPosition: "left",
+      sourcePosition: "right",
+    },
+    {
+      id: "13",
+      data: { label: `Failed Document Verification ${data.failedCount}` },
+      position: { x: 500, y: 200 },
+      parentNode: "p",
+      extent: "parent",
+      // draggable: false,
+      type: "default",
+      style: {
+        ...commonStyles,
+        backgroundColor: "red",
+        color: "white",
+      },
+      targetPosition: "left",
+      sourcePosition: "right",
+    },
+    {
+      id: "14",
+      data: { label: `Successfully Applied ${data.successCount}` },
+      position: { x: 500, y: 400 },
+      parentNode: "p",
+      extent: "parent",
+      type: "default",
+      // draggable: false,
+      style: {
+        ...commonStyles,
+        backgroundColor: "green",
+        color: "white",
+      },
+      targetPosition: "left",
+      sourcePosition: "right",
+    },
+  ];
+  const initialEdges = [
+    {
+      id: "e1-2",
+      source: "1",
+      target: "2",
+      style: {
+        ...customEdgeStyles,
+      },
+    },
+    {
+      id: "e1-new",
+      source: "1",
+      target: "new",
+      style: {
+        ...customEdgeStyles,
+      },
+    },
+    {
+      id: "e2-3",
+      source: "2",
+      target: "3",
+      style: {
+        ...customEdgeStyles,
+      },
+    },
+    {
+      id: "e2-4",
+      source: "2",
+      target: "4",
+      style: {
+        ...customEdgeStyles,
+      },
+    },
+    {
+      id: "e2-5",
+      source: "2",
+      target: "5",
+      style: {
+        ...customEdgeStyles,
+      },
+    },
+    {
+      id: "e6-7",
+      source: "6",
+      target: "7",
+      style: {
+        ...customEdgeStyles,
+      },
+    },
+    {
+      id: "e6-8",
+      source: "6",
+      target: "8",
+      style: {
+        ...customEdgeStyles,
+      },
+    },
+    {
+      id: "e6-9",
+      source: "6",
+      target: "9",
+      style: {
+        ...customEdgeStyles,
+      },
+    },
+    {
+      id: "e6-10",
+      source: "6",
+      target: "10",
+      style: {
+        ...customEdgeStyles,
+      },
+    },
+    {
+      id: "e8-11",
+      source: "8",
+      target: "11",
+      style: {
+        ...customEdgeStyles,
+      },
+    },
+    {
+      id: "e8-12",
+      source: "8",
+      target: "12",
+      style: {
+        ...customEdgeStyles,
+      },
+    },
+    {
+      id: "e12-13",
+      source: "12",
+      target: "13",
+      style: {
+        ...customEdgeStyles,
+      },
+    },
+    {
+      id: "e12-14",
+      source: "12",
+      target: "14",
+      style: {
+        ...customEdgeStyles,
+      },
+    },
+    {
+      id: "e",
+      source: "4",
+      target: "6",
+      // animated: "true",
+      label: `Scholarship ${scholarshipId}`,
+      style: {
+        ...customEdgeStyles,
+      },
+    },
+    // { id: "e13-parent", source: "13", target: "parent" },
+  ];
+
+  const rfStyle = {
+    backgroundColor: "#D0C0F7",
+  };
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
@@ -341,15 +430,17 @@ export default function Graph() {
   );
 
   return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <ReactFlow
-        nodes={nodes}
-        edges={edges}
-        onNodesChange={onNodesChange}
-        onEdgesChange={onEdgesChange}
-        onConnect={onConnect}
-        style={rfStyle}
-      />
-    </div>
+    <>
+      <div style={{ width: "100vw", height: "100vh" }}>
+        <ReactFlow
+          nodes={nodes}
+          edges={edges}
+          onNodesChange={onNodesChange}
+          onEdgesChange={onEdgesChange}
+          onConnect={onConnect}
+          style={rfStyle}
+        />
+      </div>
+    </>
   );
 }
